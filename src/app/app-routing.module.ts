005c60ebaff19d8793aac46f407b8ab717f288aa
Binshabs <binshabs567@gmail.com>
2024-06-01 16:17:45 +0530

    dashboard initiation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: MainContentComponent },
+  { path: 'reports', component: MainContentComponent }, // You can replace with actual components
+  { path: 'settings', component: MainContentComponent } // You can replace with actual components
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
